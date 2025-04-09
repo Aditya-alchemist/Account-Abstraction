9165c9bf2a30413c0abbb1d5562a20e6264101fd
Aditya41205 <157035985+Aditya41205@users.noreply.github.com>
2025-04-10 03:36:04 +0530

    first commit

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -7,14 +7,24 @@ import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED,SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 
 
 contract MinimalAccount is IAccount , Ownable {
 
-    constructor() Ownable(msg.sender){}
+    IEntryPoint private immutable  i_entryPoint;
 
-     function validateUserOp( PackedUserOperation calldata userOp, bytes32 userOpHash,uint256 missingAccountFunds) external returns (uint256 validationData)
+       modifier onlyEntryPoint() {
+        require(msg.sender == address(i_entryPoint), "only EntryPoint can call this function");
+        _;
+    }
+
+    constructor( address entrypoint ) Ownable(msg.sender){
+        i_entryPoint = IEntryPoint(entrypoint);
+    }
+
+     function validateUserOp( PackedUserOperation calldata userOp, bytes32 userOpHash,uint256 missingAccountFunds) external onlyEntryPoint returns (uint256 validationData)
      {
         
        validationData =  _validateSignature(userOp,userOpHash);
@@ -39,6 +49,11 @@ contract MinimalAccount is IAccount , Ownable {
             (bool success, ) = payable(msg.sender).call{value: missingAccountFunds,gas: type(uint256).max}("");
             require(success, "Failed to transfer funds");
         }
+    }
+
+
+    function getEntrypoint() external view returns (address) {
+        return address(i_entryPoint);
     }
 
 }
